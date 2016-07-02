2b709f34d8448f51844c06c1ffd9b3f26b2feb8f
Rplus <Rplus.tw@gmail.com>
2016-07-02 08:08:49 -0500

    Feature/85 automation with scripts (#86)
    close #85
    
    * 調整 pull Feeds from Facebook 的方式
    
    讓開啟的網址能夠自動抓到前一週週日的日期
    也提供指定日期來限定：
    ex: `npm run pullFeeds -- --date=2016-01-01`
    
    * 改寫輸出前十筆資料為 yaml 的方式
    
    調整為直接用 nodejs 處理，直接輸出檔案到資料夾裡
    
    * remove (old) useless scripts
    
    * 調整 FB feeds 收集方式為 node 自行擷取 request
    
    並自動輸出 json & 要貼到 github 的 issue 檔案
    
    * 忽略到 issue 暫用檔 & FB token
    
    * 可以自動填入 issue comment 的 bookmarklet script
    
    * 擷取投票結果改為 npm script 自動化操作
    
    * rename bookmarklet script
    
    * 加入 markdown file basic template 以加速編輯

---
+++ .weekly-feed/getData/calcTop10.js
@@ -0,0 +1,68 @@
+var fs = require('mz/fs');
+var argv = require('yargs').argv;
+var mustache = require('mustache');
+var lastSunday = require('./lastSunday').date;
+
+var date = argv.date || lastSunday;
+
+console.log('Start date: ', date);
+
+var fetchJSONFile = ($path) => {
+  return fs.readFile($path, 'utf8').then(data => JSON.parse(data)).catch(err => {
+    console.log(err);
+  });
+};
+
+var top20 = fetchJSONFile(`.weekly-feed/${date}.json`);
+var top20Voted = fetchJSONFile(`.weekly-feed/${date}-vote.json`);
+
+var yamlTpl = `- permalink: 'https://fb.com/{{ id }}'
+  time: '{{ created_time }}'
+  sharer-name: '{{ from.name }}'
+  sharer-id: '{{ from.id }}'
+  title: '{{ name }}'
+  link: '{{{ link }}}'
+  {{# cover }}
+  cover: '{{{ cover }}}'
+  {{/ cover }}
+  intro: ''
+`;
+
+var mdTpl = `---
+layout: post
+title: "Collection #0{{ week }}"
+categories: collection
+tags: weekly-popular
+publish-date: '{{ publishDate }}'
+date-since: '{{ dateSince }}'
+date-until: '{{ dateUntil }}'
+volunteer: {{ volunteer }}
+---
+
+{% include card.html %}
+`;
+
+Promise.all([top20, top20Voted]).then(v => {
+  var top20 = v[0];
+  var top20Voted = v[1];
+
+  var top10Voted = top20Voted.slice(0, 10);
+
+  var top10yaml = top10Voted.map(i => {
+    // filter matched data in top20, and render with mustache
+    return mustache.render(yamlTpl, top20.filter(j => j.id === i.postId)[0]);
+  });
+
+  var now = new Date();
+
+  var frontMatter = {
+    week: ~~((new Date(date) - new Date('2016-03-13')) / (7 * 24 * 60 * 60 * 1000)),
+    dateSince: date,
+    dateUntil: date.replace(/(\d\d)$/, a => a * 1 + 6),
+    publishDate: new Date(now - now.getTimezoneOffset() * 60 * 1000).toJSON().split('T')[0],
+    volunteer: argv.volunteer || 'Rplus'
+  };
+
+  fs.writeFile(`_data/${date}.yml`, top10yaml.join('\n'));
+  fs.writeFile(`_posts/${date}-0${frontMatter.week}.md`, mustache.render(mdTpl, frontMatter));
+});
---
+++ .weekly-feed/getData/pullFeeds.js
@@ -0,0 +1,82 @@
+var fs = require('mz/fs');
+var argv = require('yargs').argv;
+var mustache = require('mustache');
+var request = require('request');
+var FBTOKEN = require('./FBToken').token;
+
+var date = argv.date;
+
+if (!date) {
+  date = require('./lastSunday').date;
+}
+
+var issueTpl = `| power# | likes | comments | shares |
+|:------:|:-----:|:--------:|:------:|
+| # {{ order }} | {{ power.1 }} | {{ power.2 }} | {{ power.3 }} |
+* [fb permalink](https://fb.com/{{ id }}) @ \`{{ created_time }}\`
+* who: [{{{ from.name }}}](https://facebook.com/{{ from.id }})  
+![](https://graph.facebook.com/{{ from.id }}/picture?width=60&height=60)
+{{# message }}
+* intro:
+\`\`\`
+{{{ message }}}
+\`\`\`
+{{/ message }}
+{{# icon }}
+> ![]({{{ icon }}}): [{{ name }}]({{{ link }}})
+{{/ icon }}
+{{# description }}
+> {{{ description }}}
+{{/ description }}
+{{#picture}}
+> ![]({{{ picture }}})
+{{/picture}}`;
+
+console.log('Start date: ', date);
+
+request({
+  uri: `https://graph.facebook.com/521085554595481/feed?since=${+new Date(date) / 1000}&access_token=${FBTOKEN}&limit=200&fields=comments.limit(0).summary(true).filter(stream),likes.limit(0).summary(true),shares,created_time,updated_time,from,message,icon,link,name,description,picture`
+}, (err, response, body) => {
+  if (err) { console.log(err); }
+
+  var data = JSON.parse(body).data;
+
+  var since = +new Date(date);
+  var until = since + 7 * 24 * 60 * 60 * 1000;
+  var _time;
+  console.log(`week since: ${new Date(since).toJSON()}, ${since}\nweek until: ${new Date(until).toJSON()}, ${until}`);
+
+  var filteredData = data.filter((item) => {
+    _time = +new Date(item.created_time);
+    return _time >= since && _time <= until;
+  });
+
+  console.log(`The total number of feeds in this week is: ${filteredData.length}`);
+
+  filteredData.forEach(stat => {
+    var likes = stat.likes && stat.likes.summary.total_count || 0;
+    var comments = stat.comments && stat.comments.summary.total_count || 0;
+    var shares = stat.shares && stat.shares.count || 0;
+
+    stat.power = [likes * 1 + comments * 1.5 + shares * 2, likes, comments, shares];
+  });
+
+  var top20 = filteredData.sort((fa, fb) => {
+    return fb.power[0] - fa.power[0];
+  }).slice(0, 20);
+
+  var issue20 = top20.map((feed, index) => {
+    feed.order = index + 1;
+
+    if (feed.description) {
+      feed.description = feed.description.replace(/\n/gm, '\n> ');
+    }
+
+    return mustache.render(issueTpl, feed);
+  });
+
+  // fs.writeFile(`.weekly-feed/${date}-o.json`, JSON.stringify(data, null, 2));
+  // fs.writeFile(`.weekly-feed/${date}-f.json`, JSON.stringify(filteredData, null, 2));
+  fs.writeFile(`.weekly-feed/${date}.json`, JSON.stringify(top20, null, 2));
+  fs.writeFile(`.weekly-feed/issue-${date}.json`, JSON.stringify(issue20, null, 2).replace(/\#x2F\;/gm, '/'));
+});
