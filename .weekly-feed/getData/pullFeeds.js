468af7904e2ddffbc25d72ab76cc05580f7f371a
Rplus <Rplus.tw@gmail.com>
2016-07-18 03:06:53 -0500

    Feature/91 prompt parameter (#94)
    close #91 
    
    * 為 pull feeds 加入參數確認步驟
    
    * fix coding-style
    
    * 為 query voted data 加入參數確認步驟
    
    * fix coding style
    
    * 為 calcTop10 加入參數確認步驟
    
    * fix coding style
    
    * 加入已完成的提示訊息

--- .weekly-feed/getData/pullFeeds.js
+++ .weekly-feed/getData/pullFeeds.js
@@ -2,6 +2,7 @@ var fs = require('mz/fs');
 var argv = require('yargs').argv;
 var mustache = require('mustache');
 var request = require('request');
+var prompt = require('prompt');
 var FBTOKEN = require('./token.json').facebook;
 
 var date = argv.date;
@@ -32,51 +33,62 @@ var issueTpl = `| power# | likes | comments | shares |
 > ![]({{{ picture }}})
 {{/picture}}`;
 
-console.log('Start date: ', date);
+console.log(`請確認開始日期: ${date} ?`);
 
-request({
-  uri: `https://graph.facebook.com/521085554595481/feed?since=${+new Date(date) / 1000}&access_token=${FBTOKEN}&limit=200&fields=comments.limit(0).summary(true).filter(stream),likes.limit(0).summary(true),shares,created_time,updated_time,from,message,icon,link,name,description,picture`
-}, (err, response, body) => {
+prompt.get([{
+  name: 'startDate',
+  pattern: /^\d{4}(-\d{2}){2}$/,
+  default: date
+}], (err, result) => {
   if (err) { console.log(err); }
+  date = result.startDate;
 
-  var data = JSON.parse(body).data;
+  console.log('Start date: ', date);
 
-  var since = +new Date(date);
-  var until = since + 7 * 24 * 60 * 60 * 1000;
-  var _time;
-  console.log(`week since: ${new Date(since).toJSON()}, ${since}\nweek until: ${new Date(until).toJSON()}, ${until}`);
+  request({
+    uri: `https://graph.facebook.com/521085554595481/feed?since=${+new Date(date) / 1000}&access_token=${FBTOKEN}&limit=200&fields=comments.limit(0).summary(true).filter(stream),likes.limit(0).summary(true),shares,created_time,updated_time,from,message,icon,link,name,description,picture`
+  }, (err, response, body) => {
+    if (err) { console.log(err); }
 
-  var filteredData = data.filter((item) => {
-    _time = +new Date(item.created_time);
-    return _time >= since && _time <= until;
+    var data = JSON.parse(body).data;
+
+    var since = +new Date(date);
+    var until = since + 7 * 24 * 60 * 60 * 1000;
+    var _time;
+    console.log(`week since: ${new Date(since).toJSON()}, ${since}\nweek until: ${new Date(until).toJSON()}, ${until}`);
+
+    var filteredData = data.filter((item) => {
+      _time = +new Date(item.created_time);
+      return _time >= since && _time <= until;
+    });
+
+    console.log(`The total number of feeds in this week is: ${filteredData.length}`);
+
+    filteredData.forEach(stat => {
+      var likes = stat.likes && stat.likes.summary.total_count || 0;
+      var comments = stat.comments && stat.comments.summary.total_count || 0;
+      var shares = stat.shares && stat.shares.count || 0;
+
+      stat.power = [likes * 1 + comments * 1.5 + shares * 2, likes, comments, shares];
+    });
+
+    var top20 = filteredData.sort((fa, fb) => {
+      return fb.power[0] - fa.power[0];
+    }).slice(0, 20);
+
+    var issue20 = top20.map((feed, index) => {
+      feed.order = index + 1;
+
+      if (feed.description) {
+        feed.description = feed.description.replace(/\n/gm, '\n> ');
+      }
+
+      return mustache.render(issueTpl, feed);
+    });
+
+    // fs.writeFile(`.weekly-feed/${date}-o.json`, JSON.stringify(data, null, 2));
+    // fs.writeFile(`.weekly-feed/${date}-f.json`, JSON.stringify(filteredData, null, 2));
+    fs.writeFile(`.weekly-feed/${date}.json`, JSON.stringify(top20, null, 2));
+    fs.writeFile(`.weekly-feed/issue-${date}.json`, JSON.stringify(issue20, null, 2).replace(/\#x2F\;/gm, '/'));
   });
-
-  console.log(`The total number of feeds in this week is: ${filteredData.length}`);
-
-  filteredData.forEach(stat => {
-    var likes = stat.likes && stat.likes.summary.total_count || 0;
-    var comments = stat.comments && stat.comments.summary.total_count || 0;
-    var shares = stat.shares && stat.shares.count || 0;
-
-    stat.power = [likes * 1 + comments * 1.5 + shares * 2, likes, comments, shares];
-  });
-
-  var top20 = filteredData.sort((fa, fb) => {
-    return fb.power[0] - fa.power[0];
-  }).slice(0, 20);
-
-  var issue20 = top20.map((feed, index) => {
-    feed.order = index + 1;
-
-    if (feed.description) {
-      feed.description = feed.description.replace(/\n/gm, '\n> ');
-    }
-
-    return mustache.render(issueTpl, feed);
-  });
-
-  // fs.writeFile(`.weekly-feed/${date}-o.json`, JSON.stringify(data, null, 2));
-  // fs.writeFile(`.weekly-feed/${date}-f.json`, JSON.stringify(filteredData, null, 2));
-  fs.writeFile(`.weekly-feed/${date}.json`, JSON.stringify(top20, null, 2));
-  fs.writeFile(`.weekly-feed/issue-${date}.json`, JSON.stringify(issue20, null, 2).replace(/\#x2F\;/gm, '/'));
 });
--- .weekly-feed/getData/queryVote.js
+++ .weekly-feed/getData/queryVote.js
@@ -1,169 +1,187 @@
 var fs = require('mz/fs');
 var argv = require('yargs').argv;
 var jsdom = require('jsdom');
+var prompt = require('prompt');
 
-var url = `https://github.com/f2etw/weekly-collection/issues/${argv.issue || 6}`;
-
-var member = [
-  'Rplus',
-  'whalesingswee',
-  'amazingandyyy',
-  'Clementtang',
-  'erwaiyang',
-  'noootown',
-  'ryrocks'
-];
-
-var allowedReactions = ['+1', 'thinking_face', 'tada'];
-
-var weight = {
-  likes: 1,      /* 互動基數 */
-  comments: 1.5, /* 鼓勵討論 */
-  shares: 2,     /* 鼓勵擴散 */
-  vote: 3        /* 些微調整 */
-};
-
-// includes polyfill from MDN
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
-if (!Array.prototype.includes) {
-  Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
-    'use strict';
-    var O = Object(this);
-    var len = parseInt(O.length) || 0;
-    if (len === 0) {
-      return false;
+var confirmIssueNum = new Promise((resolve, reject) => {
+  prompt.get([{
+    name: 'issueNum',
+    pattern: /^\d+$/,
+    default: argv.issue || 6
+  }], (err, result) => {
+    if (err) {
+      console.log(err);
+      reject(result);
     }
-    var n = parseInt(arguments[1]) || 0;
-    var k;
-    if (n >= 0) {
-      k = n;
-    } else {
-      k = len + n;
-      if (k < 0) {k = 0;}
-    }
-    var currentElement;
-    while (k < len) {
-      currentElement = O[k];
-      if (searchElement === currentElement ||
-         (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
-        return true;
-      }
-      k++;
-    }
-    return false;
+
+    resolve(result);
+  });
+});
+
+confirmIssueNum.then((result) => {
+  var url = `https://github.com/f2etw/weekly-collection/issues/${result.issueNum}`;
+
+  var member = [
+    'Rplus',
+    'whalesingswee',
+    'amazingandyyy',
+    'Clementtang',
+    'erwaiyang',
+    'noootown',
+    'ryrocks'
+  ];
+
+  var allowedReactions = ['+1', 'thinking_face', 'tada'];
+
+  var weight = {
+    likes: 1,      /* 互動基數 */
+    comments: 1.5, /* 鼓勵討論 */
+    shares: 2,     /* 鼓勵擴散 */
+    vote: 3        /* 些微調整 */
   };
-}
 
-jsdom.env({
-  url: url,
-  done: (err, window) => {
-    if (err) { console.log(err); }
-
-    var doc = window.document;
-    var date = doc.title.match(/\d{4}-\d{2}-\d{2}/g)[0];
-
-    var box = doc.querySelector('.js-discussion');
-    var comments = [].slice.call(box.querySelectorAll('.timeline-comment-wrapper'));
-    var targetComments = comments.filter((comment) => {
-      var firstTd = comment.querySelector('tbody td');
-      return !!firstTd && /^\# \d+/.test(firstTd.textContent);
-    });
-
-    var emojisData = targetComments.map((t) => {
-      var reactionBtn = [].slice.call(t.querySelectorAll('.comment-reactions-options .reaction-summary-item'));
-
-      var avatarBoxStyle = doc.getElementById('customAvatarBoxStyle');
-      if (!avatarBoxStyle) {
-        var style = doc.createElement('style');
-        style.id = 'customAvatarBoxStyle';
-        style.innerHTML = '.customAvatarBox img {width: 20px; height: 20px; vertical-align: top; margin-left: 3px; border-radius: 3px;}';
-        doc.head.appendChild(style);
+  // includes polyfill from MDN
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+  if (!Array.prototype.includes) {
+    Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
+      'use strict';
+      var O = Object(this);
+      var len = parseInt(O.length) || 0;
+      if (len === 0) {
+        return false;
       }
+      var n = parseInt(arguments[1]) || 0;
+      var k;
+      if (n >= 0) {
+        k = n;
+      } else {
+        k = len + n;
+        if (k < 0) {k = 0;}
+      }
+      var currentElement;
+      while (k < len) {
+        currentElement = O[k];
+        if (searchElement === currentElement ||
+           (searchElement !== searchElement && currentElement !== currentElement)) { // NaN !== NaN
+          return true;
+        }
+        k++;
+      }
+      return false;
+    };
+  }
 
-      return reactionBtn.map((btn) => {
-        var who = btn.getAttribute('aria-label').split(' reacted with')[0].replace(/,| and/g, '').split(/\s+/);
+  jsdom.env({
+    url: url,
+    done: (err, window) => {
+      if (err) { console.log(err); }
 
-        return {
-          reaction: btn.value.split(/\s+/)[0],
-          who: who
-        };
+      var doc = window.document;
+      var date = doc.title.match(/\d{4}-\d{2}-\d{2}/g)[0];
+
+      var box = doc.querySelector('.js-discussion');
+      var comments = [].slice.call(box.querySelectorAll('.timeline-comment-wrapper'));
+      var targetComments = comments.filter((comment) => {
+        var firstTd = comment.querySelector('tbody td');
+        return !!firstTd && /^\# \d+/.test(firstTd.textContent);
       });
-    });
 
-    var sumTable = emojisData.map((emojis, idx) => {
-      var voteData = {};
-      var _id = targetComments[idx].querySelector('.comment-body a').href.replace('https://fb.com/', '');
+      var emojisData = targetComments.map((t) => {
+        var reactionBtn = [].slice.call(t.querySelectorAll('.comment-reactions-options .reaction-summary-item'));
 
-      console.log(idx, `https://fb.com/${_id}`);
+        var avatarBoxStyle = doc.getElementById('customAvatarBoxStyle');
+        if (!avatarBoxStyle) {
+          var style = doc.createElement('style');
+          style.id = 'customAvatarBoxStyle';
+          style.innerHTML = '.customAvatarBox img {width: 20px; height: 20px; vertical-align: top; margin-left: 3px; border-radius: 3px;}';
+          doc.head.appendChild(style);
+        }
 
-      var tds = [].slice.call(targetComments[idx].querySelectorAll('tbody td'));
-      var tdsData = tds.map((td) => {
-        return td.textContent;
-      });
+        return reactionBtn.map((btn) => {
+          var who = btn.getAttribute('aria-label').split(' reacted with')[0].replace(/,| and/g, '').split(/\s+/);
 
-      voteData.order = tdsData[0];
-      voteData.likes = tdsData[1];
-      voteData.comments = tdsData[2];
-      voteData.shares = tdsData[3];
-
-      voteData.vote = 0;
-
-      emojis.forEach((emoji) => {
-        if (allowedReactions.indexOf(emoji.reaction) === -1) { return; }
-
-        var reaction = emoji.reaction;
-        var people = emoji.who;
-
-        people.forEach((t) => {
-          voteData[t] = reaction;
+          return {
+            reaction: btn.value.split(/\s+/)[0],
+            who: who
+          };
         });
       });
 
-      Object.keys(voteData).forEach((i) => {
-        if (!member.includes(i)) { return; }
+      var sumTable = emojisData.map((emojis, idx) => {
+        var voteData = {};
+        var _id = targetComments[idx].querySelector('.comment-body a').href.replace('https://fb.com/', '');
 
-        switch (voteData[i]) {
-          case '+1':
-            voteData.vote += 1;
-            break;
+        console.log(idx, `https://fb.com/${_id}`);
 
-          case 'thinking_face':
-            voteData.vote -= 1;
-            break;
+        var tds = [].slice.call(targetComments[idx].querySelectorAll('tbody td'));
+        var tdsData = tds.map((td) => {
+          return td.textContent;
+        });
 
-          case 'tada':
-            if (i === member[0]) {
-              console.log('tada');
-              voteData.vote -= 500;
-            }
-            break;
-        }
+        voteData.order = tdsData[0];
+        voteData.likes = tdsData[1];
+        voteData.comments = tdsData[2];
+        voteData.shares = tdsData[3];
+
+        voteData.vote = 0;
+
+        emojis.forEach((emoji) => {
+          if (allowedReactions.indexOf(emoji.reaction) === -1) { return; }
+
+          var reaction = emoji.reaction;
+          var people = emoji.who;
+
+          people.forEach((t) => {
+            voteData[t] = reaction;
+          });
+        });
+
+        Object.keys(voteData).forEach((i) => {
+          if (!member.includes(i)) { return; }
+
+          switch (voteData[i]) {
+            case '+1':
+              voteData.vote += 1;
+              break;
+
+            case 'thinking_face':
+              voteData.vote -= 1;
+              break;
+
+            case 'tada':
+              if (i === member[0]) {
+                console.log('tada');
+                voteData.vote -= 500;
+              }
+              break;
+          }
+        });
+
+        voteData.calcSum = voteData.likes * weight.likes + voteData.comments * weight.comments + voteData.shares * weight.shares + voteData.vote * weight.vote;
+
+        voteData.postId = _id;
+
+        return voteData;
       });
 
-      voteData.calcSum = voteData.likes * weight.likes + voteData.comments * weight.comments + voteData.shares * weight.shares + voteData.vote * weight.vote;
+      window.sumTable = sumTable;
 
-      voteData.postId = _id;
+      window.top10 = sumTable.sort((a, b) => {
+        return b.calcSum - a.calcSum;
+      });
 
-      return voteData;
-    });
+      // console.table(window.top10);
+      // console.log('copy(top10)');
 
-    window.sumTable = sumTable;
-
-    window.top10 = sumTable.sort((a, b) => {
-      return b.calcSum - a.calcSum;
-    });
-
-    // console.table(window.top10);
-    // console.log('copy(top10)');
-
-    // fs.writeFile(`.weekly-feed/getData/.t/vote-${date}-o.html`, doc.documentElement.outerHTML);
-    // fs.writeFile(
-    //   `.weekly-feed/getData/.t/vote-${date}.html`,
-    //   targetComments.map(i => i.outerHTML).join('')
-    // );
-    fs.writeFile(
-      `.weekly-feed/${date}-vote.json`,
-      JSON.stringify(window.top10, null, 2) + '\n'
-    );
-  }
+      // fs.writeFile(`.weekly-feed/getData/.t/vote-${date}-o.html`, doc.documentElement.outerHTML);
+      // fs.writeFile(
+      //   `.weekly-feed/getData/.t/vote-${date}.html`,
+      //   targetComments.map(i => i.outerHTML).join('')
+      // );
+      fs.writeFile(
+        `.weekly-feed/${date}-vote.json`,
+        JSON.stringify(window.top10, null, 2) + '\n'
+      );
+    }
+  });
 });
